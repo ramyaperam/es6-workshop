1d3b45627816421878d4988039aa5280d5b76746
ramyaperam <peram.ramya77@gmail.com>
2023-06-12 12:28:54 -0500

    this is ramya

--- exercises/01_block-scoping.test.js
+++ exercises/01_block-scoping.test.js
@@ -1,7 +1,9 @@
 const noop = () => {}
 test('can be used in place of `var`', () => {
   // Declare bandName using 'let'
+  let bandName='Queen';
   // Declare isBestBand using 'let'
+  let isBestBand=true;
   expect(bandName).toBe('Queen')
   expect(isBestBand).toBe(true)
 })
@@ -11,7 +13,7 @@ test('can modify the value of a `let` variable even in the next block statement'
   {
     releaseName = 'ES2015'
   }
-  expect(releaseName).toBe(/* ENTER YOUR GUESS HERE */)
+  expect(releaseName).toBe('ES2015')
 })
 
 test('cannot modify the value of a `const` variable', () => {
@@ -19,7 +21,8 @@ test('cannot modify the value of a `const` variable', () => {
     // Pick your side. Do you call it ES6, or ES2015?
     // You cannot have `const` and reassign the value!
     const releaseName = 'ES6' // If you call it ES2015, then change this to let or var
-    releaseName = 'ES2015' // If you call it ES6, then remove this reassignment
+  
+    // If you call it ES6, then remove this reassignment
     return releaseName
   }
   expect(getReleaseName).not.toThrow()
@@ -28,7 +31,7 @@ test('cannot modify the value of a `const` variable', () => {
 test('is trapped inside of an `if` statement', () => {
   if (true) {
     // Change to `var` to `let`, so that b is scoped inside of the if-statement
-    var b = 1
+    let b = 1
   }
   expect(() => noop(b)).toThrow('b is not defined')
 })
@@ -36,7 +39,7 @@ test('is trapped inside of an `if` statement', () => {
 test(`can't redeclare using the same name`, () => {
   function doLoop() {
     // Change loop counter to `let` so that it is trapped inside of the loop, and can't be returned.
-    for (var i = 0; i < 10; i++) {
+    for (let i = 0; i < 10; i++) {
       /* eslint no-empty:"off" */
     }
     return i
@@ -49,7 +52,7 @@ test('means that we can start using block statements', () => {
   // BLOCK STATEMENT
   {
     // Change to `const` declaration
-    var d = 2
+    const d = 2
   }
 
   expect(() => noop('d', d)).toThrow('d is not defined')
@@ -57,20 +60,23 @@ test('means that we can start using block statements', () => {
 
 //////// Elaboration & Feedback /////////
 /*
-http://ws.kcd.im/?ws=ES6+and+Beyond&e=Block+Scoping&em=
+http://ws.kcd.im/?ws=ES6+and+Beyond&e=Block+Scoping&em=ashu.nikam26@gmail.com
 */
 test('I submitted my elaboration and feedback', () => {
-  const submitted = false // change this when you've submitted!
+  const submitted = true
+   // change this when you've submitted!
   expect(true).toBe(submitted)
 })
 ////////////////////////////////
 
 //////// EXTRA CREDIT ////////
 
-test.skip('means that we can declare constant with the same name in block statement', () => {
+test('means that we can declare constant with the same name in block statement', () => {
   // Declare a 'd' using 'const', setting the value to 5
+  const d=5;
   // BLOCK STATEMENT
   {
+    const d=10;
     // Declare a 'd' using 'const', setting the value to 10
     expect(d).toBe(10)
   }
